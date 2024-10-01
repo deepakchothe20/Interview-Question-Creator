59156b93634a13cf3f5ed099451d7bfe8ca6c89c
deepakchothe20 <deepakchothe1998@gmail.com>
2024-10-01 19:57:26 +0530

    requirement

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Genravtive AI',
+    version = '0.0.1',
+    description = 'A setuptools based setup module',
+    author = 'Deepak Chothe',
+    author_email = 'deepakchothe1998@gmail.com',
+    packages = find_packages(),
+    install_requires = [])
